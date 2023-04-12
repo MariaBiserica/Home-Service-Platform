f529093ae5d2d78eaada34a325f3a45b5047a782
MariaBiserica <mariabiserica@gmail.com>
2023-04-12 14:11:02 +0300

    Added Sign Up page, added 'Remember Me' checkbox in Login page - checkbox functionality still needs work. Added common css style to remove duplicate code from Login and SignUp css.

--- home-service-platform/src/app/components/login/login.component.ts
+++ home-service-platform/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  usernameOrEmail: string = "";
+  password: string = "";
+  rememberMe: boolean = false;
+  @ViewChild('rememberMeCheckbox')
+  rememberMeCheckbox!: ElementRef;
+  @ViewChild('usernameOrEmailInput')
+  usernameOrEmailInput!: ElementRef;
+  @ViewChild('passwordInput')
+  passwordInput!: ElementRef;
 
   type: string = "password";
   isText: boolean = false;
@@ -20,4 +30,13 @@ export class LoginComponent implements OnInit {
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
+
+  onSubmit() {
+    this.usernameOrEmail = this.usernameOrEmailInput.nativeElement.innerText;
+    this.password = this.passwordInput.nativeElement.innerText;
+    this.rememberMe = this.rememberMeCheckbox.nativeElement.checked;
+
+    console.log(this.usernameOrEmail, this.password, this.rememberMe);
+  }
+  
 }
