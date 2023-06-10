aac5821b0ffeb6dbf02b105bd0cee350c996e00f
MariaBiserica <mariabiserica@gmail.com>
2023-06-11 00:48:14 +0300

    Created  Table-Feature module to for new Table component.

--- home-service-platform/src/app/modules/app-routing.module.ts
+++ home-service-platform/src/app/modules/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from '../components/login/login.component';
+import { SignupComponent } from '../components/signup/signup.component';
 
 const routes: Routes = [
   {
@@ -12,10 +12,14 @@ const routes: Routes = [
     path: 'signup',
     component: SignupComponent
   },
+  { 
+    path: 'table', 
+    loadChildren: () => import('./table-feature.module').then(m => m.TableFeatureModule) 
+  },
   {
     path:'**',
     redirectTo: 'login'
-  }
+  },
 ];
 
 @NgModule({
--- home-service-platform/src/app/modules/app.module.ts
+++ home-service-platform/src/app/modules/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
@@ -10,13 +10,14 @@ import en from '@angular/common/locales/en';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from '../components/login/login.component';
+import { SignupComponent } from '../components/signup/signup.component';
 
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { TableFeatureModule } from './table-feature.module';
 
 
 registerLocaleData(en);
@@ -25,7 +26,7 @@ registerLocaleData(en);
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,7 +38,8 @@ registerLocaleData(en);
     NzCardModule,
     NzCheckboxModule,
     ReactiveFormsModule,
-    NzButtonModule
+    NzButtonModule,
+    TableFeatureModule,
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
